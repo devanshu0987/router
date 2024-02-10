8f35e220cbfa72ddbfc50f48db12c4cd83655fc3
Devanshu M <devansm@microsoft.com>
2024-02-10 19:36:45 +0530

    Remove stray interfaces

--- src/main/java/org/codapayments/router/instanceListSupplier/Add.java
+++
@@ -1,7 +0,0 @@
-package org.codapayments.router.instanceListSupplier;
-
-import java.net.URI;
-
-public interface Add {
-    public void add(URI uri);
-}
--- src/main/java/org/codapayments/router/instanceListSupplier/Cooldown.java
+++
@@ -1,7 +0,0 @@
-package org.codapayments.router.instanceListSupplier;
-
-import java.net.URI;
-
-public interface Cooldown {
-    public void setCooldown(URI uri, Long timestamp);
-}
