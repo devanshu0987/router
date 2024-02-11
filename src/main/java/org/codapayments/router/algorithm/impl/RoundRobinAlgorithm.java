8a78015eba2a460787a67257ce80476e9beab3d5
Devanshu M <devansm@microsoft.com>
2024-02-11 10:52:47 +0530

    Solved a lock that was not unlocked and tested echo v2 and cooldown

--- src/main/java/org/codapayments/router/algorithm/impl/RoundRobinAlgorithm.java
+++ src/main/java/org/codapayments/router/algorithm/impl/RoundRobinAlgorithm.java
@@ -1,12 +1,9 @@
 package org.codapayments.router.algorithm.impl;
 
 import org.codapayments.router.algorithm.RoutingAlgorithm;
-import org.codapayments.router.config.RoutingConfig;
 import org.codapayments.router.instanceListSupplier.ServiceInstanceListSupplier;
 
 import java.net.URI;
-import java.time.LocalDateTime;
-import java.util.List;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class RoundRobinAlgorithm implements RoutingAlgorithm {
@@ -23,6 +20,10 @@ public class RoundRobinAlgorithm implements RoutingAlgorithm {
         lock.lock();
 
         int size = supplier.get().size();
+        if(size == 0) {
+            lock.unlock();
+            return null;
+        }
         URI instanceToRouteTo = supplier.get().get(presentIndex % size);
         presentIndex++;
         presentIndex = presentIndex % size;
